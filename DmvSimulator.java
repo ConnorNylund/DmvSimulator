11d0d7572287607f7ba54a3741b8518c75508ee7
connornylund <csnylund@quinnipiac.edu>
2024-02-29 14:48:15 -0500

    made the setup for the DMV

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class DmvSimulator {
+
     public static void main(String[] args) {
-        
+
+        System.out.println("Welcome to the dmv");
+        Random random = new Random();
+        int userNumber = random. nextInt(100);
+        System.out.println("Your number is " + userNumber + ", please wait untill your number is called.");
+
+        int currentNumber = userNumber + 1;
+
+        while (true) {
+            if (currentNumber == 100)
+                currentNumber = 0;
+            System.out.println("Now serving number " + currentNumber);
+            if (currentNumber == userNumber)
+                break;
+            currentNumber++;
+        }
+        System.out.println("Your number " + userNumber + " is now being served.");
+        System.out.println("I'm sorry you do not have the requierd paperwork.");
     }
 }
