06164b96df3b800d7161ab17aefb2dadfcb450c6
connornylund <csnylund@quinnipiac.edu>
2024-03-06 12:57:30 -0500

    changed the paper work message

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -4,10 +4,10 @@ public class DmvSimulator {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the dmv");
+        System.out.println("Welcome to the DMV");
         Random random = new Random();
-        int userNumber = random. nextInt(100);
-        System.out.println("Your number is " + userNumber + ", please wait untill your number is called.");
+        int userNumber = random.nextInt(100);
+        System.out.println("Your number is " + userNumber + ", please wait until your number is called.");
 
         int currentNumber = userNumber + 1;
 
@@ -18,8 +18,15 @@ public class DmvSimulator {
             if (currentNumber == userNumber)
                 break;
             currentNumber++;
+
+            // Generate a random number between 0 and 99
+            int chance = random.nextInt(100);
+            if (chance == 0) { // 1% chance
+                System.out.println("You have all the required paperwork and are all set.");
+                break;
+            } else if (chance > 0 && chance <= 98) { // 99% chance
+                System.out.println("Are you so incompetent to not have the required paperwork?! Why can't people just bring what is required!?");
+            }
         }
-        System.out.println("Your number " + userNumber + " is now being served.");
-        System.out.println("Are you so incompetend to not have the required paperwork?! Why can't people just bring what is required!?");
     }
 }
