3883166b0ae3f3d981d597184089bca88b2f436e
connornylund <csnylund@quinnipiac.edu>
2024-03-06 13:09:50 -0500

    added a % chance

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,25 +1,33 @@
+Copy code
 import java.util.Random;
 
 public class DmvSimulator {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the dmv");
+        System.out.println("Welcome to the DMV");
         Random random = new Random();
-        int userNumber = random. nextInt(100);
-        System.out.println("Your number is " + userNumber + ", please wait untill your number is called.");
+        int userNumber = random.nextInt(100);
+        System.out.println("Your number is " + userNumber + ", please wait until your number is called.");
 
         int currentNumber = userNumber + 1;
 
         while (true) {
-            if (currentNumber == 100)
+            if (currentNumber == 200)
                 currentNumber = 0;
-            System.out.println("Now serving number " + currentNumber);
+            System.out.println("Now serving number: " + currentNumber);
             if (currentNumber == userNumber)
                 break;
             currentNumber++;
+
+            // Generate a random number between 0 and 99
+            int chance = random.nextInt(100);
+            if (chance == 0) { // 1% chance
+                System.out.println("You have all the required paperwork and are all set.");
+                break;
+            } else if (chance > 0 && chance <= 98) { // 99% chance
+                System.out.println("Are you so incompetent to not have the required paperwork?! Why can't people just bring what is required!?");
+            }
         }
-        System.out.println("Your number " + userNumber + " is now being served.");
-        System.out.println("Are you so incompetend to not have the required paperwork?! Why can't people just bring what is required!?");
     }
 }
